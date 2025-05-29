1ccf4bb30caddde2f912b2fcddc1b86587355ad1
MATHEUS BILITARDO ABIB <97857@pmogi.local>
2025-05-29 09:22:46 -0300

    Configuração index.ph

--- forms/config.php
+++ forms/config.php
@@ -1,7 +1,7 @@
 <?php
 // config.php (não versionado no GitHub)
 return [
-    'SENDGRID_API_KEY' => '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',
+    'SENDGRID_API_KEY' => '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',
     'EMAIL_FROM' => 'bilitardomatheus@gmail.com',
     'EMAIL_TO' => 'bilitardomatheus@gmail.com'
 ];
