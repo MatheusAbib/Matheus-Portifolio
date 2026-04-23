39cbe0f15369471a4a66f1479815d07795ad3d8b
MatheusAbib <matheus.abib.ma@gmail.com>
2026-04-23 13:06:54 -0300

    feat: ajuste em link de projetos

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -58,7 +58,7 @@ const Portfolio = () => {
       stack: "fullstack",
       image: "/assets/img/portfolio/Organizador-Arquivos.png",
       github: "https://github.com/MatheusAbib/Organizador-de-Arquivos",
-      live: "https://organizador-arquivos.cleverapps.io/",
+      live: "https://descending-carb-chips-hugo.trycloudflare.com/organizador/",
       tags: ["HTML", "CSS", "JavaScript", "Node.Js", "MySQL"]
     },
     {
